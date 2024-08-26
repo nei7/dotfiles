8ea5dbcd04ba5a94fe283f3bf6f864880964cc0f
nei7 <franciszek.szarek2@gmail.com>
2024-08-26 09:59:22 +0000

    refactor: volume

--- .config/eww/scripts/get_vol.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ $1 == "speaker" ]
-then
-    amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }' | tr -d '%' | head -1
-elif [ $1 == "mic" ]
-then
-    amixer sget Capture | grep 'Left:' | awk -F'[][]' '{ print $2 }' | tr -d '%' | head -1
-fi
